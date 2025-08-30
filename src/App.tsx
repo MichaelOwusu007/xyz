78187a0c9d20946d8773a430128a1651273e134c
Mickerl_Codes <nanayawwiafe007@gmail.com>
2025-08-29 22:41:41 -0700

    xyz
    xyz

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => (
           <Route path="/pricing" element={<Pricing />} />
           <Route path="/support" element={<Support />} />
           <Route path="/contact" element={<Contact />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
